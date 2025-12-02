e7b5272d02047a11f87285b8b2deeccee7006fec
Tobe-Wang <hzbeq@qq.com>
2025-12-02 10:54:24 +0800

    feat(config): 启用Sa-Token拦截器并注释说明
    - 启用Sa-Token拦截器以支持注解式鉴权
    - 添加注释说明拦截器模式与AOP模式的区别
    - 提醒拦截器模式和AOP模式不可同时集成以免重复校验

--- sa-springboot/src/main/java/cn/zhaofd/saspringboot/config/SpringMvcConfig.java
+++ sa-springboot/src/main/java/cn/zhaofd/saspringboot/config/SpringMvcConfig.java
@@ -19,8 +19,9 @@ public class SpringMvcConfig implements WebMvcConfigurer {
      */
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-//        // 注册Sa-Token拦截器，打开注解式鉴权功能
-//        // 默认拦截器模式有一个缺点无法在Controller层以外的代码使用进行校验，使用AOP插件便可以在任意层级使用注解鉴权
+        // 注册Sa-Token拦截器，打开注解式鉴权功能
+        // 默认拦截器模式有一个缺点无法在Controller层以外的代码使用进行校验，使用AOP插件便可以在任意层级使用注解鉴权
+        // 拦截器模式和AOP模式不可同时集成，否则会在Controller层发生一个注解校验两次的bug
 //        registry.addInterceptor(new SaInterceptor()).addPathPatterns("/**");
     }
 }
